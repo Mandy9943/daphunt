474b7788d11791bb497e8200a59629d2e8841959
Mandy9943 <armandoc9943@gmail.com>
2023-11-09 16:12:34 -0500

    fix erros

--- utils/functions/urls.ts
+++ utils/functions/urls.ts
@@ -1,14 +1,5 @@
-import { routeNames } from "@/config/routes";
-
 export const getWebUrl = (path: string = ""): string => {
   return `${window.location.origin + path}`;
-};
-
-export const isActiveRoute = (routeName: string, location: string) => {
-  if (routeName === "/") {
-    return location === routeName || location === routeNames.swapLp;
-  }
-  return location.includes(routeName);
 };
 
 export function checkNestedRoute(passedRoute: string): boolean {
