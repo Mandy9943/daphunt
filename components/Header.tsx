af365408e6ea96d3970b84656e58c57c4ff2c97d
Mandy9943 <armandoc9943@gmail.com>
2023-11-09 08:18:43 -0500

    update logos

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     <>
       <nav className="max-w-7xl  md:text-sm flex sm:flex-row flex-col justify-between items-center gap-6">
         <Link href="/" className="flex items-center gap-3">
-          <Image src="/newIcon.svg" alt="logo" width={30} height={30} />
+          <Image src="/logo.png" alt="logo" width={30} height={30} />
           <div className="font-bold text-lg">Daphunt</div>
         </Link>
         <div className="text-gray-700 dark:text-gray-300 sm:max-w-7xl max-w-[200px]">
