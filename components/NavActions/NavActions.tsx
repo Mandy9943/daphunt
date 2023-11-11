c0ebc5fa0404965aa6ce344e85888e7732eb8add
Mandy9943 <armandoc9943@gmail.com>
2023-11-11 10:02:06 -0500

    put message connect wallet to submit dapp

--- components/NavActions/NavActions.tsx
+++ components/NavActions/NavActions.tsx
@@ -2,14 +2,24 @@
 
 import Login from "@/components/Login/Login";
 import { Button } from "@/components/ui/button";
+import useAuthentication from "@/hooks/useAuthentication";
 import { Dot, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 const NavbarActions = () => {
   const { setTheme, theme } = useTheme();
-
+  const router = useRouter();
+  const { isLoggedIn } = useAuthentication();
+  const onSubmitDappClick = () => {
+    if (isLoggedIn) {
+      router.push("/new");
+    } else {
+      toast.error("Connect your wallet to submit a dApp");
+    }
+  };
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center gap-x-[4px] " variant={"outline"}>
@@ -19,10 +29,8 @@ const NavbarActions = () => {
         </span>
       </Button>
 
-      <Button variant={"outline"} asChild>
-        <Link href={"/new"} className="whitespace-nowrap">
-          Submit Dapp
-        </Link>
+      <Button variant={"outline"} onClick={onSubmitDappClick}>
+        Submit Dapp
       </Button>
 
       {/* <div className="hidden sm:block">
