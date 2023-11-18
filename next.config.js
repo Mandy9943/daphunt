72e2b9632ec7204677617f2d2f75b22fe86ce4d1
Mandy9943 <armandoc9943@gmail.com>
2023-11-18 11:09:03 -0500

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["id.maiar.com", "flzavuqgiwduroylglww.supabase.co"],
+    domains: [
+      "id.maiar.com",
+      "flzavuqgiwduroylglww.supabase.co",
+      "cxuglqomjolmilyyiclj.supabase.co",
+    ],
   },
   transpilePackages: ["@multiversx/sdk-dapp"],
   webpack: (config) => {
